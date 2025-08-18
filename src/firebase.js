714e215e9bed864c8229f6567488fc36534befaa
Tai Nguyen <112757316+taismyname@users.noreply.github.com>
2025-08-18 14:11:38 +0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG34WY1EM7UWlWK-Ko87Ewrw5hMD51Ib8",
+  authDomain: "toeic-app-alpha.firebaseapp.com",
+  projectId: "toeic-app-alpha",
+  storageBucket: "toeic-app-alpha.firebasestorage.app",
+  messagingSenderId: "795144074395",
+  appId: "1:795144074395:web:bca98509e3e01461b8823e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
