353ad4132d45f068dc48bdb59b315204eca27926
Tai Nguyen <112757316+taismyname@users.noreply.github.com>
2025-08-20 22:17:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
-console.log('Main.jsx loaded'); // Thêm log để debug
+console.log('Main.jsx loaded'); // Debug log
 const root = document.getElementById('root');
 if (!root) {
   console.error('Root element not found');
 } else {
+  console.log('Rendering App...'); // Debug log
   createRoot(root).render(
     <BrowserRouter>
       <App />
