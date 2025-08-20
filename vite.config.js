16da8dd0241eb35bb33da6e953e00d3ac034d6a3
Tai Nguyen <112757316+taismyname@users.noreply.github.com>
2025-08-20 20:27:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.js', '**/*.jsx'], // Giữ dòng này để fix MIME type
+  assetsInclude: ['**/*.js', '**/*.jsx'], // Giữ để fix MIME type
   build: {
     outDir: 'dist',
     rollupOptions: {
